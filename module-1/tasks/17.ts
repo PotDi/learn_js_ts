af7e8fc646edbe83e0b68de991047cb8432f7534
dmitriy.potapov <dimasikbasket@mail.ru>
2022-03-14 22:57:27 +0300

    Правки в 17 module-1 и в 1,2 module-2

--- module-1/tasks/17.ts
+++ module-1/tasks/17.ts
@@ -1,3 +1,7 @@
 // Создать функцию для генерации селектора для выбора элементов по data-at="id=header". сигнатура (id: string) => string,
 // Пример foo('header') вернет [data-at*="id=header"]
 // Прочитать что такое селектор
+
+function select(id: string) {
+    console.log(select)
+}
--- module-2/tasks/01.ts
+++ module-2/tasks/01.ts
@@ -1,2 +1,7 @@
 // Вывести список ролей через for
+export{}
+
 const roles = ['owner', 'admin', 'reviewer']
+for (const index in roles) {
+    console .log(index, roles[index]) //не совсем понятно как это работает
+}
