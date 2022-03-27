e62e913f73edc7900f3c935cee393818d2843017
Dima <dimasikbasket@mail.ru>
2022-03-27 22:36:49 +0300

    Решены задачи 3,4,6

--- module-2/tasks/03.ts
+++ module-2/tasks/03.ts
@@ -1,6 +1,12 @@
 // Дописать функцию для подсчета суммы нечетных чисел в массиве
 function sumOdd(arr: number[]): number {
-    return 0
+    let sum = 0
+    for (let array of arr ) {
+        if (array % 2 === 1) { //насколько я правильно понял??
+            sum += array
+        }
+    }
+    return sum
 }
 
 const nums = [1, 2, 3]
--- module-2/tasks/06.ts
+++ module-2/tasks/06.ts
@@ -1,1 +1,9 @@
 // Cоздать объект user c полями name, age. Вывести фразу "Меня зовут <Имя>, мне <Возраст>"
+export{}
+
+let user = {
+    name: 'Дима',
+    age: 34
+}
+
+console.log(`Меня зовут ${user.name}, мне ${user.age}`)
